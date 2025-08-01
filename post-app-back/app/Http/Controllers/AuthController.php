03e0b6b73e0e05d223bcf9ed7bfe34d3d20fa5d8
Chamodya B Herath <134180331+CA-Creation@users.noreply.github.com>
2025-08-01 10:28:13 +0530

    done login

---
+++ post-app-back/app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showRegister() {
+    return view('auth.register');
+}
+
+public function register(Request $request) {
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|confirmed|min:6',
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+}
+
+public function showLogin() {
+    return view('auth.login');
+}
+
+public function login(Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $remember = $request->has('remember');
+
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember)) {
+        $request->session()->regenerate();
+        return redirect('/dashboard');
+    }
+
+    return back()->withErrors(['email' => 'Invalid credentials.']);
+}
+public function logout(Request $request) {
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect('/login');
+}
+
+}
